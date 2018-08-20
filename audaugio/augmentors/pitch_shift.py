65baead0755699a91e11723fd6ca75f19e3ffc4b
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-08-20 11:57:06 -0500

    Add pitch shift augmentation

---
+++ audaugio/augmentors/pitch_shift.py
@@ -0,0 +1,12 @@
+import librosa
+
+from audaugio.augmentors.generics import Augmentation
+
+
+class PitchShiftAugmentation(Augmentation):
+    def __init__(self, steps):
+        super().__init__(replaces=False)
+        self.steps = steps
+
+    def augment(self, audio, sr):
+        return [librosa.effects.pitch_shift(audio, sr, self.steps)]
