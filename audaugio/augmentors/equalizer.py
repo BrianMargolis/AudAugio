a03f6261c01b4cb2d1762adcb90a7e5324a83cda
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-08-20 14:11:56 -0500

    Add handling for missing SoX installation

--- audaugio/augmentors/equalizer.py
+++ audaugio/augmentors/equalizer.py
@@ -2,6 +2,7 @@ import os
 
 import librosa
 import sox
+from sox import SoxError
 
 from audaugio.augmentors.generics import Augmentation
 
@@ -16,11 +17,25 @@ class EqualizerAugmentation(Augmentation):
 
     def augment(self, audio, sr):
         librosa.output.write_wav(self.input_file, audio, sr=sr)
-        self.transformer.build(self.input_file, self.output_file)
+        try:
+            self.transformer.build(self.input_file, self.output_file)
+        except SoxError as e:
+            self.cleanup()
+            if 'sox: command not found' in e.args[0]:
+                raise OSError("You need a working installation of SoX to use this augmentation.\nIf you haven't installed it, "
+                              "go to http://sox.sourceforge.net/ for a download link. Otherwise, double check your path variables.")
+            else:
+                raise e
         audio, sr = librosa.load(self.output_file, sr=sr)
         self.cleanup()
         return [audio]
 
     def cleanup(self):
-        os.remove(self.input_file)
-        os.remove(self.output_file)
+        try:
+            os.remove(self.input_file)
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove(self.output_file)
+        except FileNotFoundError:
+            pass
