d4e189c9b4693574abfc4702b60babb786b76f87
Brian Margolis <Brian.M.Margolis@gmail.com>
2018-08-22 15:35:01 -0500

    Update docs, switch to rst for README

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../'))
-print(sys.path)
 
 # -- General configuration ------------------------------------------------
 
@@ -87,7 +86,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,12 @@
-.. AudAugio documentation master file, created by
-   sphinx-quickstart on Mon Aug 20 15:34:00 2018.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+Documentation
+==================
 
-Welcome to AudAugio's documentation!
-====================================
+* :ref:`modindex`
+
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+.. include:: ../README.rst
